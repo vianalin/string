94c10e4eca09eebc152df808cf26cda1d02d3d98
Viana Lin <vianalin@pop-os.localdomain>
2021-10-08 00:28:26 -0400

    tested strcat

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "mystring.h"
 
 int main() {
 
@@ -9,25 +10,30 @@ int main() {
 	char s2[100] = "hello";
 	char s3[100] = "goodbye";
 	
-	printf("start strings: \n");
-	printf("s1: [%s] \n", s1);
-	printf("s2: [%s] \n", s2);
-	printf("s3: [%s] \n", s3);
+	//printf("start strings: \n");
+	//printf("s1: [%s] \n", s1);
+	//printf("s2: [%s] \n", s2);
+	//printf("s3: [%s] \n", s3);
 	
-	printf("\n");
+	//printf("\n");
 	
-	printf("testing strlen(s2): \n");
-	printf("[standard]: \t %lu \n", strlen(s2));
-	printf("[mine]: \t %d \n", mystrlen(s2));
+	//printf("testing strlen(s2): \n");
+	//printf("[standard]: \t %lu \n", strlen(s2));
+	//printf("[mine]: \t %d \n", mystrlen(s2));
+	
+	//printf("\n");
+  	
+	//printf("testing strcpy(s1, s2): \n");
+	//printf("[standard]: \t [%s] \n", strcpy(s1, s2));
+	//printf("[mine]: \t [%s] \n", mystrcpy(s1, s2));
+	
+	//printf("\n");
+	
+	printf("testing strcat(s1, s3): \n");
+	printf("[standard]: \t [%s] \n", strcat(s1, s3));
+	s1[5] = 0;
+	printf("[mine]: \t [%s] \n", mystrcat(s1, s3));
 }
 
-int mystrlen(char *s) {
-	int len = 0; 
-	while(*s) {
-		s++;
-		len++;
-	} 
-	return len;
-}
 
 
