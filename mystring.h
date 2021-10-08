d8bc16b5c5bccf3b02db9f312c3a3c5e3737f913
Viana Lin <vianalin@pop-os.localdomain>
2021-10-08 00:28:49 -0400

    .h

--- mystring.h
+++ mystring.h
@@ -1,2 +1,6 @@
 int mystrlen(char *s);
+char * mystrcpy( char *dest, char *source);
+char * mystrcat( char *dest, char *source);
+int mystrcmp(char *s1, char *s2);
+char * mystrchr(char *s, char c) ;
 
