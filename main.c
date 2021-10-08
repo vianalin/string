01f5f766a9107c49df5400342721b6820e2370e1
Viana Lin <vianalin@pop-os.localdomain>
2021-10-07 23:06:28 -0400

    tested strlen

--- main.c
+++ main.c
@@ -3,3 +3,31 @@
 #include <string.h>
 
 
+int main() {
+
+	char s1[100];
+	char s2[100] = "hello";
+	char s3[100] = "goodbye";
+	
+	printf("start strings: \n");
+	printf("s1: [%s] \n", s1);
+	printf("s2: [%s] \n", s2);
+	printf("s3: [%s] \n", s3);
+	
+	printf("\n");
+	
+	printf("testing strlen(s2): \n");
+	printf("[standard]: \t %lu \n", strlen(s2));
+	printf("[mine]: \t %d \n", mystrlen(s2));
+}
+
+int mystrlen(char *s) {
+	int len = 0; 
+	while(*s) {
+		s++;
+		len++;
+	} 
+	return len;
+}
+
+
